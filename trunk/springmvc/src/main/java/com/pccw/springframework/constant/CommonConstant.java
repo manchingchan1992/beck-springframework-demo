cb535072992cc0307d5d499a6f1caaf6c35c7eed
beck.bq.lu@gmail.com <beck.bq.lu@gmail.com@04613604-a6fd-3c26-cffa-d49f373097df>
2012-03-22 16:06:59 +0000

    增加spring security

--- trunk/springmvc/src/main/java/com/pccw/springframework/constant/CommonConstant.java
+++ trunk/springmvc/src/main/java/com/pccw/springframework/constant/CommonConstant.java
@@ -2,4 +2,7 @@ package com.pccw.springframework.constant;
 
 public class CommonConstant {
 	public static String EMAIL_SUFFIX = "pccw.com";
+	
+	public static String USER_ACCOUNT_ACTIVE = "AC";
+	public static String USER_ACCOUNT_INACTIVE = "IN";
 }
---
+++ trunk/springmvc/src/main/java/com/pccw/springframework/dao/impl/HibernateOfficeUserDAO.java
@@ -0,0 +1,39 @@
+package com.pccw.springframework.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import com.pccw.springframework.dao.OfficeUserDAO;
+import com.pccw.springframework.repository.OfficeUser;
+
+@Component(value="officeUserDao")
+public class HibernateOfficeUserDAO implements OfficeUserDAO{
+	
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	@SuppressWarnings("unchecked")
+	public OfficeUser getOfficeUserByLoginId(final String loginId) {
+		OfficeUser user = (OfficeUser)hibernateTemplate.execute(new HibernateCallback() {
+			public Object doInHibernate(Session session)
+					throws HibernateException, SQLException {
+				String HQL  = "FROM OfficeUser usr WHERE usr.loginId = :loginId";
+				Query query = session.createQuery(HQL);
+				query.setString("loginId", loginId);
+				List<OfficeUser> usrs = query.list();
+				return CollectionUtils.isEmpty(usrs) ? null : usrs.get(0);
+			}
+		});
+		return user;
+	}
+
+}
