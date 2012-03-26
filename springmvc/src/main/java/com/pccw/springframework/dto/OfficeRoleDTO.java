848f1d9b17c37c1e4585d8ed26e35947a439f40c
Beck.bq.lu <beck.bq.lu@gmail.com>
2012-03-26 16:31:30 +0000

    修补权限问题

--- springmvc/src/main/java/com/pccw/springframework/dto/OfficeRoleDTO.java
+++ springmvc/src/main/java/com/pccw/springframework/dto/OfficeRoleDTO.java
@@ -1,6 +1,45 @@
 package com.pccw.springframework.dto;
 
-public class OfficeRoleDTO extends BaseDTO{
+public class OfficeRoleDTO extends BaseDTO {
 	private static final long serialVersionUID = 1L;
 
+	private String systemRoleRefNum;
+
+	private String roleId;
+
+	private String roleName;
+
+	private String roleDesc;
+
+	public String getSystemRoleRefNum() {
+		return systemRoleRefNum;
+	}
+
+	public void setSystemRoleRefNum(String systemRoleRefNum) {
+		this.systemRoleRefNum = systemRoleRefNum;
+	}
+
+	public String getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getRoleDesc() {
+		return roleDesc;
+	}
+
+	public void setRoleDesc(String roleDesc) {
+		this.roleDesc = roleDesc;
+	}
 }
