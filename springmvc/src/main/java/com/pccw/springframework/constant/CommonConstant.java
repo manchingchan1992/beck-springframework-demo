b55cb222fcdf2812598c65934217e706cafdd9bd
Beck.bq.lu <beck.bq.lu@gmail.com>
2012-07-11 10:00:53 +0000

    增加HDIV

--- springmvc/src/main/java/com/pccw/springframework/constant/CommonConstant.java
+++ springmvc/src/main/java/com/pccw/springframework/constant/CommonConstant.java
@@ -1,26 +1,28 @@
 package com.pccw.springframework.constant;
 
 public class CommonConstant {
-	public static String EMAIL_SUFFIX = "pccw.com";
+	public static final String EMAIL_SUFFIX = "pccw.com";
 	
-	public static String USER_ACCOUNT_ACTIVE = "AC";
-	public static String USER_ACCOUNT_INACTIVE = "IN";
+	public static final String USER_ACCOUNT_ACTIVE = "AC";
+	public static final String USER_ACCOUNT_INACTIVE = "IN";
 	
-	public static String ANOYMOUS_OFFICE_USER = "ANONY";
-	public static String ANOYMOUS_USER_EMAIL = "Beck.BQ.Lu@pccw.com";
+	public static final String ANOYMOUS_OFFICE_USER = "ANONY";
+	public static final String ANOYMOUS_USER_EMAIL = "Beck.BQ.Lu@pccw.com";
 	
-	public static String INBOX = "I";
-	public static String OUTBOX = "O";
-	public static String RECYCLE = "R";
+	public static final String INBOX = "I";
+	public static final String OUTBOX = "O";
+	public static final String RECYCLE = "R";
 	
-	public static String YES = "Y";
-	public static String NO = "N";
+	public static final String YES = "Y";
+	public static final String NO = "N";
 	
-	public static String STRING_ENCODE_BY_MD5 = "MD5";
-	public static String STRING_ENCODE_BY_DEFAULT = "BASE64";
+	public static final String STRING_ENCODE_BY_MD5 = "MD5";
+	public static final String STRING_ENCODE_BY_DEFAULT = "BASE64";
 	
-	public static String EMPTY_STRING = "";
+	public static final String EMPTY_STRING = "";
 	
-	public static String ACTIVE = "AC";
-	public static String INACTIVE = "IN";
+	public static final String ACTIVE = "AC";
+	public static final String INACTIVE = "IN";
+	
+	public static final boolean ENABLE_HDIV = true;
 }
